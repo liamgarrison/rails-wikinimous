9c0a078f34f03ec857d7296610d096f7e41394c4
Liam Garrison <liamgarrison@gmail.com>
2019-05-10 14:22:50 +0100

    set up article MVC and seed

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  # GET /articles
+  def index
+    @articles = Article.all
+  end
+
+  # GET /articles/1
+  def show
+  end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # GET /articles/1/edit
+  def edit
+  end
+
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+  end
+
+  # PATCH/PUT /articles/1
+  def update
+    @article.update(article_params)
+  end
+
+  # DELETE /articles/1
+  def destroy
+    @article.destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def article_params
+    params.fetch(:article, {})
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  Article.create(title: Faker::Lorem.sentence(10), content: Faker::Lorem.sentence(100))
+end
